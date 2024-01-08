00ff5bdf0ae511455ecf91eb07c868578b8756b5
metatech-programmer <97856988+metatech-programmer@users.noreply.github.com>
2024-01-08 12:21:00 -0500

    update route

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import './App.css'
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
 import Footer from './components/Footer/FooterPage'
+import { Router } from 'wouter'
 
 function App () {
   return (
-    <div className='w-screen h-screen font-sans dark:text-slate-200 overflow-auto bg-gradient-to-t from-gray-800 to-50% to-gray-950'>
+    <Router>
+      <div className='w-screen h-screen font-sans dark:text-slate-200 overflow-auto bg-gradient-to-t from-gray-800 to-50% to-gray-950'>
         <Header></Header>
         <Main></Main>
         <Footer></Footer>
       </div>
+    </Router>
   )
 }
 
