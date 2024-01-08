2969bad32aed7c2f9b7c8a41ca4464d011fa189f
metatech-programmer <97856988+metatech-programmer@users.noreply.github.com>
2024-01-08 12:43:19 -0500

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import './App.css'
-import Header from './components/Header/Header'
-import Main from './components/Main/Main'
-import Footer from './components/Footer/FooterPage'
-import { Router } from 'wouter'
+import "./App.css";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Footer from "./components/Footer/FooterPage";
+import { Router } from "wouter";
 
-function App () {
-  return (
-    <Router>
-      <div className='w-screen h-screen font-sans dark:text-slate-200 overflow-auto bg-gradient-to-t from-gray-800 to-50% to-gray-950'>
-        <Header></Header>
-        <Main></Main>
-        <Footer></Footer>
-      </div>
-    </Router>
-  )
+function App() {
+	return (
+		<Router>
+			<div className="w-screen h-screen font-sans dark:text-slate-200 overflow-auto bg-gradient-to-t from-gray-800 to-50% to-gray-950">
+				<Header />
+				<Main/>
+				<Footer/>
+			</div>
+		</Router>
+	);
 }
 
-export default App
+export default App;
