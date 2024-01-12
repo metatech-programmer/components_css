01191d739d16ee1f8e68025af26dac64d452cab9
metatech-programmer <97856988+metatech-programmer@users.noreply.github.com>
2024-01-12 11:44:20 -0500

    update main an app

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/FooterPage";
 import PopUp from "./components/Popup/PopUp";
-import { Router, Switch } from "wouter";
-import { useEffect } from "react";
+import { Router } from "wouter";
 
 function App() {
 	return (
@@ -12,9 +11,7 @@ function App() {
 			<div className="w-screen h-screen font-sans dark:text-slate-200 overflow-scroll bg-gradient-to-t from-gray-800 to-50% to-gray-95 ">
 				<PopUp />
 				<Header />
-				<Switch>
-					<Main />
-				</Switch>
+				<Main />
 				<Footer />
 			</div>
 		</Router>
